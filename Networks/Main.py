0c96c71142ef014356d52839f034e461ae6f1724
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-20 00:40:30 +0700

    Modified:
    - Fix RandomStreams error - must put MRG_RandomStreams after HiddenLayer to override shared.RandomStream declared in HiddenLayer
    - Create train and test function for fine-tuning

--- Networks/Main.py
+++ Networks/Main.py
@@ -1,34 +1,35 @@
 import Utils.DataHelper as DataHelper
 import os
-from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 from PIL import Image
+from Utils import CostFHelper
 from Utils.FilterHelper import *
 
 # Import layers
 from Layers.RBMHiddenLayer import *
-
+from Layers.HiddenLayer import *
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 # HYPER PARAMETERS
 DATASET_NAME = '../Dataset/mnist.pkl.gz'
-BATCH_SIZE = 1
+BATCH_SIZE = 10
 VALIDATION_FREQUENCY = 50000
 VISUALIZE_FREQUENCY = 5000
 
 # NETWORKS HYPER PARAMETERS
 HIDDEN_LAYERS_SIZES = [1000, 1000, 1000]
-NUM_CHAINS = 20
+NUM_CHAINS = 15
 NUM_OUT = 10
 
 # PRETRAIN HYPER PARAMETERS
 PRETRAINING_SAVE_PATH = '../Pretrained/pretrain_stage.pkl'
-PRETRAINING_EPOCH = 15
-PRETRAINING_LEARNING_RATE = 0.001
+PRETRAINING_EPOCH = 100
+PRETRAINING_LEARNING_RATE = 0.01
 PRETRAINING_SAVE_FREQUENCY = 5000
 
 # FINE-TUNING HYPER PARAMETERS
-TRAINING_SAVE_PATH = '../Pretrained/training_stage.pkl'
-TRAINING_EPOCH = 1000
-TRAINING_LEARNING_RATE = 0.1
+FINETUNING_SAVE_PATH = '../Pretrained/training_stage.pkl'
+FINETUNING_EPOCH = 50
+FINETUNING_LEARNING_RATE = 0.1
 
 def DBN():
     #########################################
@@ -65,8 +66,10 @@ def DBN():
     LearningRate = T.scalar('LearningRate', dtype = 'float32')
     X = T.matrix('X')
     X2D = X.reshape((BATCH_SIZE, 28 * 28))
+    Y = T.ivector('Y')
 
     rbmLayers = []
+    hiddenLayers = []
     for idx in range(len(HIDDEN_LAYERS_SIZES)):
         if idx == 0:
             inputSize = 28 * 28
@@ -76,7 +79,7 @@ def DBN():
         if idx == 0:
             layerInput = X2D
         else:
-            layerInput = rbmLayers[-1].Output
+            layerInput = hiddenLayers[-1].Output
 
         persistentChain = theano.shared(
             numpy.zeros((BATCH_SIZE, HIDDEN_LAYERS_SIZES[idx]), dtype=theano.config.floatX),
@@ -94,18 +97,27 @@ def DBN():
         )
 
         rbmLayers.append(rbm)
+
+        hiddenLayer = HiddenLayer(
+            rng          = rng,
+            input        = layerInput,
+            numIn        = inputSize,
+            numOut       = HIDDEN_LAYERS_SIZES[idx],
+            W            = rbm.W,
+            b            = rbm.hBias
+        )
+        hiddenLayers.append(hiddenLayer)
     # Create last layer
     inputSize = HIDDEN_LAYERS_SIZES[-1]
-    layerInput = rbmLayers[-1].Output
-    rbm = RBMHiddenLayer(
+    layerInput = hiddenLayers[-1].Output
+    hiddenLayer = HiddenLayer(
         rng          = rng,
-        theanoRng    = theanoRng,
         input        = layerInput,
-        numVisible   = inputSize,
-        numHidden    = NUM_OUT,
-        learningRate = LearningRate
+        numIn        = inputSize,
+        numOut       = NUM_OUT,
+        activation   = T.nnet.softmax
     )
-    rbmLayers.append(rbm)
+    hiddenLayers.append(hiddenLayer)
 
     ################################################
     #           Calculate cost function            #
@@ -122,11 +134,38 @@ def DBN():
                 X: trainSetX[Index * BATCH_SIZE : (Index + 1) * BATCH_SIZE]
             }
         )
-        rbmLayerFuncs.append(trainFunc)
+        rbmLayerFuncs.append([trainFunc])
 
     # Create train function to train entire model (fine-tuning stage)
     params = [] # Get all params from all rbm layers
-    [params.extend(rbmLayer.Params) for rbmLayer in rbmLayers]
+    [params.extend(hiddenLayer.Params) for hiddenLayer in hiddenLayers]
+    output = hiddenLayers[-1].Output
+    cost = CostFHelper.CrossEntropy(output, Y)
+    grads = T.grad(cost, params)
+    updates = [(param, param - LearningRate * grad)
+               for (param, grad) in zip(params, grads)]
+    # Train function
+    trainFunc = theano.function(
+        inputs   = [Index, LearningRate],
+        outputs  = [cost],
+        updates  = updates,
+        givens   = {
+            X: trainSetX[Index * BATCH_SIZE: (Index + 1) * BATCH_SIZE],
+            Y: trainSetY[Index * BATCH_SIZE: (Index + 1) * BATCH_SIZE],
+        }
+    )
+    # Test function
+    pred = T.argmax(output)
+    error = CostFHelper.Error(pred, Y)
+    testFunc = theano.function(
+        inputs  = [Index],
+        outputs = [error],
+        givens  = {
+            X: testSetX[Index * BATCH_SIZE: (Index + 1) * BATCH_SIZE],
+            Y: testSetY[Index * BATCH_SIZE: (Index + 1) * BATCH_SIZE],
+        }
+    )
+    fineTuningFunc = [trainFunc, testFunc]
 
     #########################################
     #      PRETRAINING STAGE                #
@@ -168,24 +207,43 @@ def DBN():
     #########################################
     #      FINE-TUNING STAGE                #
     #########################################
-    # for epoch in range(NUM_EPOCHS):
-    #     meanCost = []
-    #     for batchIndex in range(nTrainBatchs):
-    #         cost = trainFunc(batchIndex, LEARNING_RATE)
-    #         meanCost.append(cost)
-    #
-    #     print ('Epoch = %d, cost = %f' % (epoch, numpy.mean(meanCost)))
-    #
-    #     # Construct and save image of filter
-    #     image = Image.fromarray(
-    #         tile_raster_images(
-    #             X            =  rbm.W.get_value(borrow=True).T,
-    #             img_shape    = (28, 28),
-    #             tile_shape   = (10, 10),
-    #             tile_spacing = (1, 1)
-    #         )
-    #     )
-    #     image.save('filters_at_epoch_%i.png' % (epoch))
+    # Load save model
+    if os.path.isfile(PRETRAINING_SAVE_PATH):
+        file = open(PRETRAINING_SAVE_PATH)
+        [rbmLayer.LoadModel(file) for rbmLayer in rbmLayers]
+        file.close()
+    shape = [(28, 28), (50, 20), (50, 20)]
+    for idx, rbmLayer in enumerate(rbmLayers):
+        image = Image.fromarray(tile_raster_images(
+            X=rbmLayer.Params[0].get_value(borrow=True).T,
+            img_shape=shape[idx], tile_shape=(10, 10),
+            tile_spacing=(1, 1)))
+        image.save('filters_corruption_30_%d.png' % (idx))
+
+    # Training
+    print ('Start fine-tuning stage...')
+    for epoch in range(FINETUNING_EPOCH):
+        iter += BATCH_SIZE
+        cost = fineTuningFunc[0](trainBatchIdx, FINETUNING_LEARNING_RATE)
+
+        if iter % VISUALIZE_FREQUENCY == 0:
+            print ('Epoch = %d, iteration = %d ' % (epoch, iter))
+            print ('      Cost fine-tuning = %f ' % (cost[0]))
+
+        if iter % VALIDATION_FREQUENCY == 0:
+            validError = 0
+            print ('Validate current model ')
+            for validBatchIdx in range(nValidBatchs):
+                validError += fineTuningFunc[1](validBatchIdx)[0]
+            validError /= (nValidBatchs)
+
+            if (validError < bestError):
+                bestError = validError
+                print ('Save model ! Sum cost = %f ' % (validError))
+                file = open(FINETUNING_SAVE_PATH, 'wb')
+                [hiddenLayer.SaveModel(file) for hiddenLayer in hiddenLayers]
+                file.close()
+    print ('Start fine-tuning stage. Done!')
 
 if __name__ == '__main__':
     DBN()
